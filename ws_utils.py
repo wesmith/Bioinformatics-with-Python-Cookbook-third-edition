f2ad30a2ad6978e0b6959377f31735c50b8679e2
wesmith <smiwarsky@gmail.com>
2022-11-10 23:05:36 -0700

    module with utility functions

---
+++ ws_utils.py
@@ -0,0 +1,14 @@
+# ws_utils.py
+# WESmith 11/10/22
+
+
+def attrs(obj, skip=True, token='__'):
+    '''
+    Convenience function to print an objects attributes.
+    '''
+    attr = ['OBJECT TYPE: {}'.format(type(obj))]
+    for k in dir(obj):
+        if skip and k.__contains__(token): continue
+        attr.append(k)
+    return attr
+
